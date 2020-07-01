51d7cf9b470ecb6963f1ece655af2e3aef2fcb7c
searchs <ola.ajibode@gmail.com>
2020-07-01 21:33:08 +0100

    proper documentation example added

---
+++ python/README.md
@@ -0,0 +1,7 @@
+__Python__
+1.  Black [```black .```]
+
+
+Automatic check using `make` e.g. ```make checklist```
+
+
---
+++ python/playground.py
@@ -0,0 +1,42 @@
+class Point(object):
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+
+def one_function(name):
+    """
+    >>> one_function('black')
+    'received Black'
+    """
+    return "received {0}".format(name.title)
+
+
+def locate(latitude: float, longitude: float) -> Point:
+    """Find an object in the map by its coordinates"""
+
+
+print(locate.__annotations__)
+
+
+# Better documentation
+def data_from_response(response: dict) -> dict:
+    """If the response is OK, return itspayload.
+
+    - response: A dict like:
+
+    {
+        "status" : 200 #<int>
+        "timestamp": "...." # ISO format string of the current date time
+        "payload" : { ... } # dict with the returned data
+    }
+
+    - Raises:
+    - ValueError if the HTTP status is != 200
+    """
+    if response["status"] != 200:
+        raise ValueError
+    return {"data" : response["payload"]}
+
+
+print(help(data_from_response))
