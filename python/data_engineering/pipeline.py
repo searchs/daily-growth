07ee8d701ed83fe489d8171780e412ecd4820a6b
searchs <ola.ajibode@gmail.com>
2020-07-07 18:18:01 +0100

    example log added

--- python/data_engineering/pipeline.py
+++ python/data_engineering/pipeline.py
@@ -1,5 +1,13 @@
-class Pipeline(object):
-    pass
+class Pipeline:
+    def __init__(self):
+        self.tasks = []
+
+    def task(self):
+        def inner(func):
+            self.tasks.append(func)
+            return func
+
+        return inner
 
 
 pipeline = Pipeline()
@@ -7,7 +15,14 @@ pipeline = Pipeline()
 
 @pipeline.task()
 def first_task(x):
-    return x + 1
+    return x ** 2
+
+
+print(pipeline.tasks)
+# pipeline = Pipeline()
+# @pipeline.task()
+# def first_task(x):
+#     return x + 1
 
 
 @pipeline.task(depends_on=first_task)
@@ -28,15 +43,16 @@ def catch_error(func):
     def inner(*args):
         try:
             return func(*args)
-            
+
         except Exception as e:
             return e
+
     return inner
-        
-        
+
 
 @catch_error
 def throws_error():
-    raise Exception('Throws Error')
-     
+    raise Exception("Throws Error")
+
+
 print(throws_error())
